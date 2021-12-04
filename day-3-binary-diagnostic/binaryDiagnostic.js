fa3bafd9b5732feb7582f81364452692a19e24ec
AlsoPeters <kyle.fire@gmail.com>
2021-12-04 20:45:06 +0900

    finished day 3 pt. 1

--- day-3-binary-diagnostic/binaryDiagnostic.js
+++ day-3-binary-diagnostic/binaryDiagnostic.js
@@ -1,48 +1,65 @@
-const fs = require('fs');
+const fs = require("fs");
 
-let input = fs.readFileSync('testInput.txt', 'utf-8');
+let input = fs.readFileSync("input.txt", "utf-8");
 
-input = input.split('\n');
-//input = input.split('\r');
-console.log(input);
+input = input.split("\n");
 
 let gammaRate = [];
 let epsilonRate = [];
 
-let gammaZeroCounter = 0;
-let gamaOneCounter = 0;
+let gammaZeroCounter = [];
+let gammaOneCounter = [];
 
-const resetGammaCounters = () => {
-  gammaZeroCounter = 0;
-  gamaOneCounter = 0;
-};
+const stringLength = input[0].split("");
+stringLength.forEach(() => {
+  gammaZeroCounter.push(0);
+  gammaOneCounter.push(0);
+});
 
 for (let i = 0; i < input.length; i++) {
   let element = input[i];
 
-  let myArray = element.trimEnd();
-  console.log(myArray);
-
   for (let j = 0; j < element.length; j++) {
-    if (element.charAt(j) === '0') {
-      gammaZeroCounter += 1;
+    if (element.charAt(j) === "0") {
+      gammaZeroCounter[j] += 1;
+    } else {
+      gammaOneCounter[j] += 1;
     }
-
-    if (element.charAt(j) === '1') {
-      gamaOneCounter += 1;
-    }
-
-    console.log('Gamma zero counter is:', gammaZeroCounter);
-
-    console.log('Gamma one counter is:', gamaOneCounter);
-  }
-  if (gammaZeroCounter < gamaOneCounter) {
-    gammaRate.push(0);
-    resetGammaCounters();
-  } else {
-    gammaRate.push(1);
-    resetGammaCounters();
   }
 }
 
-console.log('Gamma rate array is:', gammaRate);
+// this will make our Gamma rate and Epsilon rate arrays
+for (k = 0; k < input[0].length; k++) {
+  if (gammaZeroCounter[k] > gammaOneCounter[k]) {
+    gammaRate.push(0);
+    epsilonRate.push(1);
+  } else {
+    gammaRate.push(1);
+    epsilonRate.push(0);
+  }
+}
+
+// turning our Gamma and Epsilon arrays into numbers
+let gammaRateNumber = gammaRate.join("");
+let epsilonRateNumber = epsilonRate.join("");
+
+// turn our Gamma & Epsilon rate numbers into decimals
+let gammaDecimal = parseInt(gammaRateNumber, 2);
+let epsilonDecimal = parseInt(epsilonRateNumber, 2);
+
+let powerConsumption = gammaDecimal * epsilonDecimal;
+
+console.log(
+  "Gamma Zero Counter:",
+  gammaZeroCounter,
+  "Gamma One Counter:",
+  gammaOneCounter
+);
+
+console.log("Gamma rate is:", gammaRate);
+console.log("Epsilon rate is:", epsilonRate);
+console.log("Gamma rate to a number is:", gammaRateNumber);
+console.log("Gamma rate decimal is:", gammaDecimal);
+console.log("Epsilon rate to a number is:", epsilonRateNumber);
+console.log("Epsilon rate decimal is:", epsilonDecimal);
+console.log("The power consumption is:", powerConsumption);
